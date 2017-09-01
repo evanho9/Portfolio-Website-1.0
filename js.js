81ac73612a8abec44f07fd865466e62313cde241
Evan Ho <evanho9@live.com>
2017-09-01 00:22:50 -0700

    Added animation and improved layout

---
+++ js.js
@@ -0,0 +1,12 @@
+var id = setInterval(frame, 1000);
+var bool = true;
+var elem = document.getElementById("blink");
+function frame() {
+  if (bool) {
+    elem.style.display = "flex";
+    bool = false;
+  } else {
+    elem.style.display = "none";
+    bool = true;
+  }
+}
